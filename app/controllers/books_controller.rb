87fb0472584f4234e391c09bffe0214c0540b451
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-06 04:47:01 +0000

    [Finish]5-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
 
+before_action :ensure_correct_book, only: [:edit, :update]
+
   def show
     @book = Book.find(params[:id])
+    @book_new =Book.new
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,15 +37,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_book
+    @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
